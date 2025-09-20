9f61b1fab1af14b0525a212bcb48c4d15cfd3bfb
Oleh Yatsenko <yatsenko.prog@gmail.com>
2025-09-20 22:21:44 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import styles from './App.module.css';
-import CafeInfo from '../CafeInfo/CafeInfo';
-import VoteOptions from '../VoteOptions/VoteOptions';
-import VoteStats from '../VoteStats/VoteStats';
-import Notification from '../Notification/Notification';
-import { Votes, VoteType } from '../../types/votes';
+import CafeInfo from './components/CafeInfo/CafeInfo';
+import VoteOptions from './components/VoteOptions/VoteOptions';
+import VoteStats from './components/VoteStats/VoteStats';
+import Notification from './components/Notification/Notification';
+import { Votes, VoteType } from './types/votes';
 
 const App = () => {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
