6ef04999dfd68caa61792745954063c8fd9f0914
Oleh Yatsenko <yatsenko.prog@gmail.com>
2025-09-20 22:05:54 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/app/App';
-import './index.css'
+import App from '/src/components/app/App';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
